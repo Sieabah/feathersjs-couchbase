a93b854abec0f72842b3f35fd7f447a8b1de1ecb
Chris S <christopher.sidell@gmail.com>
2018-02-28 17:30:00 -0500

    Add organization and tests

---
+++ test/querybuilder.test.js
@@ -0,0 +1,184 @@
+'use strict';
+
+const path = require('path');
+const { expect } = require('chai');
+const { QueryBuilder, QueryError } = require(path.join(path.dirname(path.dirname(__dirname)), 'src', 'services', 'querybuilder'));
+
+describe('Couchbase QueryBuilder', () => {
+  /**
+   *
+   * @type {QueryBuilder}
+   */
+  let Query = null;
+
+  beforeEach(() => void (Query = new QueryBuilder()));
+
+  it('Adds select query', () => {
+    Query.select('*');
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.be.a('string');
+    expect(query).to.include('SELECT');
+    expect(query).to.include('*');
+  });
+
+  it('Joins multiple selected fields', () => {
+    Query.select('name', 'username');
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.not.include('*');
+    expect(query).to.include('name,username');
+  });
+
+  it('Should have default wildcard select', () => {
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.be.a('string');
+    expect(query).to.include('SELECT');
+    expect(query).to.include('*');
+  });
+
+  it('Should bucket-scope select statements', () => {
+    Query = new QueryBuilder('testbucket');
+    Query.select('one', 'two');
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('`testbucket`.one');
+  });
+
+  it('Should not bucket-scope wildcard', () => {
+    Query = new QueryBuilder('testbucket');
+    Query.select('*');
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('`testbucket`.*');
+  });
+
+  it('Should append overwrite selects from initial query', () => {
+    Query.select('one');
+    Query.select('two');
+
+    const { query, values } = Query.build();
+
+    expect(query).to.include('two');
+    expect(query).to.not.include('one');
+  });
+
+  it('Should add fail on empty skip parameter', () => {
+    expect(() => Query.skip()).to.throw(QueryError);
+  });
+
+  it('Should add skip parameter', () => {
+    Query.skip(1);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.include('SELECT');
+    expect(query).to.include('OFFSET');
+    expect(query).to.include(1);
+  });
+
+  it('Should interpret skip', () => {
+    Query.interpret({
+      $skip: 1
+    });
+
+    const { query, values } = Query.build();
+    expect(query).to.include('OFFSET');
+    expect(query).to.include(1);
+  });
+
+  it('Should add fail on empty limit parameter', () => {
+    expect(() => Query.limit()).to.throw(QueryError);
+  });
+
+  it('Should add limit parameter', () => {
+    Query.limit(1);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.include('SELECT');
+    expect(query).to.include('LIMIT');
+    expect(query).to.include(1);
+  });
+
+  it('Should interpret limits', () => {
+    Query.interpret({
+      $limit: 1
+    });
+
+    const { query, values } = Query.build();
+    expect(query).to.include('LIMIT');
+    expect(query).to.include(1);
+  });
+
+  it('Should skip and limit', () => {
+    Query.skip(5);
+    Query.limit(5);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.include('LIMIT');
+    expect(query).to.include('OFFSET');
+    expect(query).to.include('SELECT');
+  });
+
+  it('Should interpret limit & skip', () => {
+    Query.interpret({
+      $limit: 1,
+      $skip: 1
+    });
+
+    const { query, values } = Query.build();
+
+    expect(query).to.include('LIMIT');
+    expect(query).to.include('OFFSET');
+    expect(query).to.include('SELECT');
+  });
+
+  it('Should add sort trait', () => {
+    Query.sort('age');
+
+    const { query, values } = Query.build();
+
+    expect(query).to.include('ORDER BY');
+    expect(values).to.include('age');
+    expect(query).to.include('ASC');
+  });
+
+  it('Should interpret sorting', () => {
+    Query.interpret({
+      $sort: {
+        age: 1
+      }
+    });
+
+    const { query, values } = Query.build();
+
+    expect(query).to.include('ORDER BY');
+    expect(values).to.include('age');
+    expect(query).to.include('ASC');
+  });
+
+  it('Should throw error when passed nothing', () => {
+    expect(() => Query.sort()).to.throw(QueryError);
+  });
+
+  it('Should have desc order', () => {
+    Query.sort('age', 'desc');
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('DESC');
+  });
+});
---
+++ test/whereclause.test.js
@@ -0,0 +1,167 @@
+'use strict';
+
+const path = require('path');
+const { expect } = require('chai');
+const { QueryBuilder, QueryError } = require(path.join(path.dirname(path.dirname(__dirname)), 'src', 'services', 'querybuilder'));
+
+describe('Couchbase QueryBuilder Where Clauses', () => {
+  /**
+   *
+   * @type {QueryBuilder}
+   */
+  let Query = null;
+
+  beforeEach(() => void (Query = new QueryBuilder()));
+
+  it('Should build where query', () => {
+    const $query = {
+      roomId: {
+        $in: [2, 5]
+      }
+    };
+
+    Query.interpret($query);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('WHERE');
+    expect(query).to.include('IN');
+    expect(values).to.include($query.roomId.$in);
+  });
+
+  it('Should understand $gt query', () => {
+    const $query = {
+      roomId: {
+        $gt: 1
+      }
+    };
+
+    Query.interpret($query);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('>');
+  });
+
+  it('Should understand $gte query', () => {
+    const $query = {
+      roomId: {
+        $gte: 1
+      }
+    };
+
+    Query.interpret($query);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('>=');
+  });
+
+  it('Should understand $lt query', () => {
+    const $query = {
+      roomId: {
+        $lt: 1
+      }
+    };
+
+    Query.interpret($query);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('<');
+  });
+
+  it('Should understand $lte query', () => {
+    const $query = {
+      roomId: {
+        $lte: 1
+      }
+    };
+
+    Query.interpret($query);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('<=');
+  });
+
+  it('Should understand $in Query', () => {
+    const $query = {
+      roomId: {
+        $in: [2, 5]
+      }
+    };
+
+    Query.interpret($query);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('IN');
+    expect(values).to.include($query.roomId.$in);
+  });
+
+  it('Should understand $nin Query', () => {
+    const $query = {
+      roomId: {
+        $nin: [2, 5]
+      }
+    };
+
+    Query.interpret($query);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('NOT IN');
+    expect(values).to.include($query.roomId.$nin);
+  });
+
+  it('Should understand $ne Query', () => {
+    const $query = {
+      roomId: {
+        $ne: true
+      }
+    };
+
+    Query.interpret($query);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('!=');
+  });
+
+  it('Should understand attributes', () => {
+    const $query = {
+      roomId: 3
+    };
+
+    Query.interpret($query);
+
+    const { query, values } = Query.build();
+
+    expect(query).to.be.ok;
+    expect(query).to.include('=');
+    expect(values).to.include($query.roomId);
+    expect(query).to.include('roomId');
+  });
+
+  it('Should throw error for $or', () => {
+    const $query = {
+      $or: [
+        {roomId: {$ne: true}},
+        {val: 3}
+      ]
+    };
+
+    Query.interpret($query);
+
+    expect(() => Query.build()).to.throw();
+  });
+});
