fcb329fc40109aa3bf4c13fecfaca1b4fd5301cf
Chris S <christopher.sidell@gmail.com>
2018-09-06 11:25:08 -0700

    Update service adapter to match feathers Crow
    Crow disabled multi-access by default

--- test/service.test.js
+++ test/service.test.js
@@ -6,7 +6,7 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 
 // const Promise = require('bluebird');
-const { BadRequest, NotFound } = require('@feathersjs/errors');
+const { BadRequest, NotFound, NotImplemented } = require('@feathersjs/errors');
 const couchbase = require('couchbase-promises').Mock;
 const createCouchService = require('../lib');
 const { CouchService } = createCouchService;
@@ -94,6 +94,65 @@ describe('Couchbase Adapter', function () {
       });
   });
 
+  it('Should not create multiple data by default', () => {
+    const data = {
+      id: 'foo',
+      foo: 'bar'
+    };
+
+    return Service.create([data, data])
+      .then(() => {
+        throw new Error('Created data');
+      })
+      .catch((err) => {
+        expect(err).to.be.instanceOf(BadRequest);
+      });
+  });
+
+  it('Should create multiple data when enabled', () => {
+    const data1 = {
+      id: 'foo1',
+      foo: 'bar1'
+    };
+    const data2 = {
+      id: 'foo2',
+      foo: 'bar2'
+    };
+
+    const _service = new CouchService({
+      ...ServiceConfig,
+      multi: true
+    });
+
+    return _service.create([data1, data2])
+      .then((results) => {
+        expect(results).to.be.a('array');
+        expect(results).to.have.length(2);
+      });
+  });
+
+  it('Should create multiple data `create` is enabled', () => {
+    const data1 = {
+      id: 'foo1',
+      foo: 'bar1'
+    };
+    const data2 = {
+      id: 'foo2',
+      foo: 'bar2'
+    };
+
+    const _service = new CouchService({
+      ...ServiceConfig,
+      multi: ['create']
+    });
+
+    return _service.create([data1, data2])
+      .then((results) => {
+        expect(results).to.be.a('array');
+        expect(results).to.have.length(2);
+      });
+  });
+
   it('Should create and retrieve data', () => {
     const data = {
       id: 'foo',
@@ -124,6 +183,47 @@ describe('Couchbase Adapter', function () {
         return Service.remove(data.uuid)
           .then(Service.get(data.uuid))
           .catch((err) => { expect(err).to.be.instanceOf(NotFound); });
+      });
+  });
+
+  it('Should not allow multi-remove by default', () => {
+    const data = {
+      id: 'foo',
+      foo: 'bar'
+    };
+
+    return Service.create(data)
+      .then(() => {
+        return Service.remove(null, { query: { foo: 'bar' } })
+          .then(() => {
+            throw new Error('Allowed mutli-access');
+          })
+          .catch((err) => {
+            expect(err).to.be.instanceOf(BadRequest);
+          });
+      });
+  });
+
+  it('Should throw NotImplemented on multi-remove', () => {
+    const data = {
+      id: 'foo',
+      foo: 'bar'
+    };
+
+    const _service = new CouchService({
+      ...ServiceConfig,
+      multi: ['create', 'remove']
+    });
+
+    return _service.create(data)
+      .then(() => {
+        return _service.remove(null, { query: { foo: 'bar' } })
+          .then(() => {
+            throw new Error('Implemented');
+          })
+          .catch((err) => {
+            expect(err).to.be.instanceOf(NotImplemented);
+          });
       });
   });
 
@@ -178,6 +278,76 @@ describe('Couchbase Adapter', function () {
       });
   });
 
+  it('Should not patch multi data by default', () => {
+    const data = {
+      id: 'foo',
+      foo: 'bar'
+    };
+
+    return Service.create(data)
+      .then(() => {
+        const reversed = data.foo.split().reverse().join('');
+        return Service.patch(null, { foo: reversed }, { query: { id: data.id } })
+          .then(() => {
+            throw new Error('Allowed multi-access by default');
+          })
+          .catch((err) => {
+            expect(err).to.be.instanceOf(BadRequest);
+          });
+      });
+  });
+
+  it.skip('Should patch multi data when enabled', () => {
+    const elements = [];
+
+    for (let i = 0; i < 10; i++) {
+      elements.push({
+        id: 'foo' + i,
+        foo: 'bar'
+      });
+    }
+
+    const _service = new CouchService({
+      ...ServiceConfig,
+      multi: ['create', 'patch']
+    });
+
+    return _service.create(elements)
+      .then(() => {
+        return _service.patch(null, { bar: 'added' }, { query: { foo: 'bar' } })
+          .then((results) => {
+            expect(results).to.be.a('array');
+          });
+      });
+  });
+
+  it('Should give not implemented error for multi-patch', () => {
+    const elements = [];
+
+    for (let i = 0; i < 10; i++) {
+      elements.push({
+        id: 'foo' + i,
+        foo: 'bar'
+      });
+    }
+
+    const _service = new CouchService({
+      ...ServiceConfig,
+      multi: ['create', 'patch']
+    });
+
+    return _service.create(elements)
+      .then(() => {
+        return _service.patch(null, { bar: 'added' }, { query: { foo: 'bar' } })
+          .then(() => {
+            throw new Error('Implemented');
+          })
+          .catch((err) => {
+            expect(err).to.be.instanceOf(NotImplemented);
+          });
+      });
+  });
+
   it('Should update data', () => {
     const data = {
       id: 'foo',
@@ -190,6 +360,25 @@ describe('Couchbase Adapter', function () {
           .then((el) => {
             expect(el.id).to.equal(undefined);
             expect(el.foo).to.equal(reversed);
+          });
+      });
+  });
+
+  it('Should not allow null ID on update', () => {
+    const data = {
+      id: 'foo',
+      foo: 'bar'
+    };
+
+    return Service.create(data)
+      .then(() => {
+        const reversed = data.foo.split().reverse().join('');
+        return Service.update(null, { foo: reversed }, { query: { id: data.id } })
+          .then(() => {
+            throw new Error('Allowed null ID');
+          })
+          .catch((err) => {
+            expect(err).to.be.instanceOf(BadRequest);
           });
       });
   });
